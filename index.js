842766fb287252639ea893a90cecd1c6a8543867
jdyer <>
2021-06-09 00:15:03 +0000

    Fixed the first str.slice index.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let str = "LaunchCode";
 
 //a) Use string methods to remove the first three characters from the string and add them to the end.
 //Hint - define another variable to hold the new string or reassign the new string to str.
-let newStr = (str.slice (3, 9) + str.slice(0, 3));
+let newStr = (str.slice (3, 10) + str.slice(0, 3));
 
 //Use a template literal to print the original and modified string in a descriptive phrase.
 console.log(`${str} is not spelled ${newStr}.\n`);
